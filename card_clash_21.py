a6c71236be270d29b7289ba1d10c00c2534ab171
AnirudhKrushnakumar <krushnakumara29@mcmsnj.net>
2026-03-23 21:59:35 -0400

    Added game logic (play_game function)

--- card_clash_21.py
+++ card_clash_21.py
@@ -62,12 +62,59 @@ def show_hand(player_name, hand, hide_first_card=False):
 
 def play_game():
     """Main game loop managing turns, user input, and winner logic."""
-    # TODO: Implement game flow
-    pass
+    # Defines variables
+    deck = create_deck()
+    shuffle_deck(deck)
+    player_hand = []
+    dealer_hand = []
+    
+    # Deals the initial two cards per player
+    for i in range(2):
+        player_hand.append(deal_card(deck))
+        dealer_hand.append(deal_card(deck))
+    
+    # Shows both the Dealer's and Player's hands
+    show_hand("Dealer", dealer_hand, hide_first_card=True)
+    show_hand("Player", player_hand)
+    
+    # Input whether the Player wants to hit or stand
+    while calculate_score(player_hand) < 21:
+        choice = input("Hit or Stand: ").strip().lower()
+        if choice == "stand":
+            break
+        elif choice == "hit":
+            player_hand.append(deal_card(deck))
+            show_hand("Player", player_hand)
+            if calculate_score(player_hand) > 21: # Sends message if the player ends up busting
+                print("The Player has bust, Dealer wins.")
+                return
+        else:
+            print("Please enter either 'Hit' or 'Stand'.")
+    
+    # Shows Dealer's hand, now that the player has gone
+    show_hand("Dealer", dealer_hand)
+    
+    # Adds AI for Dealer, making them stand once they reach or exceed a total score of 17
+    while calculate_score(dealer_hand) < 17:
+        dealer_hand.append(deal_card(deck))
+        show_hand("Dealer", dealer_hand)
+    
+    # Recalculates and defines both Player and Dealer scores
+    player_score = calculate_score(player_hand)
+    dealer_score = calculate_score(dealer_hand)
+    
+    # The possible game endings, other than the Player busting as defined above
+    if dealer_score > 21:
+        print("The Dealer has bust, Player wins.")
+    elif player_score > dealer_score:
+        print("The Player has won.")
+    elif dealer_score > player_score:
+        print("The Dealer has won.")
+    else:
+        print("The game has tied.")
 
 if __name__ == "__main__":
     play_game()
 
 
 
-shuffle_deck(create_deck())
