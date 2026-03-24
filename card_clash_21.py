61eb7d08d08b7afee373df74387ef8ed4ebb3acd
AnirudhKrushnakumar <krushnakumara29@mcmsnj.net>
2026-03-23 21:52:50 -0400

    Added score calculation and show hand functions

--- card_clash_21.py
+++ card_clash_21.py
@@ -33,13 +33,32 @@ def calculate_score(hand):
     Requirement: If the score is over 21 and the hand contains an Ace, 
     reduce the score by 10 until the score is <= 21 or no Aces remain.
     """
-    # TODO: Implement scoring logic and Ace adjustment
-    pass
+    # Keeps track of the score and number of aces using variables, along with logic/protection against aces
+    score = 0
+    aces = 0
+    for rank, suit in hand:
+        score = score + values[rank]
+        if rank == "Ace":
+            aces = aces + 1
+    
+    while score > 21 and aces > 0:
+        score = score - 10
+        aces = aces - 1
+    return score
 
 def show_hand(player_name, hand, hide_first_card=False):
     """Prints the formatted hand and current score for the user."""
-    # TODO: Print cards. If hide_first_card is True, obscure the first card.
-    pass
+    # Display's the current player, score, and if they have a hidden card or not, along with what cards exactly they have
+    print(player_name + "'s hand: ")
+    if hide_first_card:
+        print("Hidden")
+        for rank, suit in hand[1:]:
+            print(rank + " of " + suit)
+        print("Score: " + str(calculate_score(hand[1:])) + ", along with hidden card")
+    else:
+        for rank, suit in hand:
+            print(rank + " of " + suit)
+        print("Score: " + str(calculate_score(hand)))
 
 def play_game():
     """Main game loop managing turns, user input, and winner logic."""
